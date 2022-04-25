0601eab14fb41e6ae19d4514a7706fd89adde42f
JC Gurango <jc@jcgurango.com>
2022-04-25 20:44:23 +0800

    Set linked by field

--- erpnext/hr/doctype/linked_document/linked_document.py
+++ erpnext/hr/doctype/linked_document/linked_document.py
@@ -2,7 +2,16 @@
 # For license information, please see license.txt
 
 # import frappe
+import frappe
 from frappe.model.document import Document
 
 class LinkedDocument(Document):
-	pass
+	def db_insert(self):
+		if not self.linked_by:
+			self.linked_by = frappe.session.user
+		super().db_insert()
+
+	def db_update(self):
+		if not self.linked_by:
+			self.linked_by = frappe.session.user
+		super().db_update()
