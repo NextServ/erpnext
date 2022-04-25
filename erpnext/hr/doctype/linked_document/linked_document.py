9c9f9fbe8edc8a9d785cfa3e71906b0826952a0c
JC Gurango <jc@jcgurango.com>
2022-04-25 20:49:07 +0800

    Set date field

--- erpnext/hr/doctype/linked_document/linked_document.py
+++ erpnext/hr/doctype/linked_document/linked_document.py
@@ -3,15 +3,21 @@
 
 # import frappe
 import frappe
+from frappe import utils
 from frappe.model.document import Document
 
 class LinkedDocument(Document):
-	def db_insert(self):
+	def set_basic_fields(self):
 		if not self.linked_by:
 			self.linked_by = frappe.session.user
+
+		if not self.date:
+			self.date = utils.nowdate()
+
+	def db_insert(self):
+		self.set_basic_fields()
 		super().db_insert()
 
 	def db_update(self):
-		if not self.linked_by:
-			self.linked_by = frappe.session.user
+		self.set_basic_fields()
 		super().db_update()
