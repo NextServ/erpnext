2aec2be8b5fbeffb207729445a129ddf5d08e9c3
Jeff Chavez <jeff.chavez0828@gmail.com>
2025-05-09 11:21:02 +0800

    Print attendance.leave to test str > int error

--- erpnext/hr/doctype/attendance_calculation/attendance_calculation.py
+++ erpnext/hr/doctype/attendance_calculation/attendance_calculation.py
@@ -333,7 +333,8 @@ class AttendanceCalculation(Document):
 
 								if not working_hours and not leave and not overtime:
 									attendance.status = 'Rest day'
-							
+         
+							frappe.msgprint(f"attendance.leave: {attendance.leave}")
 							if attendance.leave > 0:
 								if attendance.working_hours > 0 or attendance.overtime > 0:
 									attendance.status = 'Half Day'
